33b81f0cdd7d9e7bce7e643de2da59010e3c72ba
RameshNCR <ncramesh2000@gmail.com>
2022-10-13 15:39:40 +0530

    previousExpense Altr

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     {id:1,d:new Date(2000,8,23),p:"Jwel Loan",r:"20000"},
     {id:2,d:new Date(2001,7,28),p:"House Loan",r:"100000"},
     {id:3,d:new Date(2002,5,21),p:"Grocery ",r:"1500"},
-    {id:4,d:new Date(2003,6,11),p:"Agriculture Loan",r:"150000"},
+    {id:4,d:new Date(2003,6,11),p:"Agri Loan",r:"150000"},
     {id:5,d:new Date(2004,11,1),p:"college fees",r:"10000"},
     {id:6,d:new Date(2005,9,31),p:"Bike Loan",r:"140000"}
   ]
